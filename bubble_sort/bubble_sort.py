315f60e396d1d39e989a9733700e50c7ba45f6c8
Ryo Nishimura <wv.nsmr@gmail.com>
2019-06-11 09:58:24 +0900

    Modify break if not exchange

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,15 +1,27 @@
 #!/usr/bin/env python
+import time
+import random
 
 def bubble_sort(slist):
     for i in range(len(slist) - 1):
+        isChange = False
         for j in range(len(slist) - 1):
             if slist[j] > slist[j+1]:
                 slist[j], slist[j+1] = slist[j+1], slist[j]
+                isChange = True
+
+        if not(isChange):
+            break
 
     return slist
 
 
 if __name__ == '__main__':
-    print(bubble_sort([1,2,3,4,5,6,7,8,9]))
-    print(bubble_sort([3,1,2,9,6,5,7,8,4]))
-    print(bubble_sort([99,45,96,43,62,0,60,4,23,19,13,99,28,1,2,3,9,2]))
+    #slist = [i for i in range(10000, 1, -1)]
+    slist = [i for i in range(1, 5000)]
+    #slist = [random.randint(0, 3000) for i in range(1, 3000)]
+    #print(slist)
+    start = time.time()
+    bubble_sort(slist)
+    print(time.time() - start)
+
